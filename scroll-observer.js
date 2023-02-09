9d23db5ae8c30e92babd7199f841a4066e2e183c
VioStill <emiklibi195@gmail.com>
2023-02-09 20:14:36 +0100

    fertig

---
+++ scroll-observer.js
@@ -0,0 +1,26 @@
+var animatingClassName = 'animate__animated';
+
+var observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2
+};
+
+function intersectionCallback(intersectingEntries) {
+  for (var j = 0; j < intersectingEntries.length; j++) {
+    if (intersectingEntries[j].isIntersecting && intersectingEntries[j].intersectionRatio > observerOptions.threshold) {
+      if (intersectingEntries[j].target && intersectingEntries[j].target.classList) {
+        var animationClassName = intersectingEntries[j].target.dataset.animationclass;
+        intersectingEntries[j].target.classList.add(animatingClassName, animationClassName);
+      }
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var observer = new IntersectionObserver(intersectionCallback, observerOptions);
+  var elementsAnimatedOnVisibility = document.getElementsByClassName('animate--on-visibility');
+  for (var i = 0; i < elementsAnimatedOnVisibility.length; i++) {
+    observer.observe(elementsAnimatedOnVisibility[i]);
+  }
+});
