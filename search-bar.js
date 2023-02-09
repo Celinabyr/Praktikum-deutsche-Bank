23661e574f657b154a554bdc613faebcb5ea398b
Celinabyr <celina-maria.bayer@web.de>
2023-02-09 22:05:56 +0100

    fertige Webseite

---
+++ search-bar.js
@@ -0,0 +1,25 @@
+function openModal() {
+    searchModal.classList.add('is-active');
+    searchInput.focus()
+}
+function closeModal($) {
+    searchModal.classList.remove('is-active');
+    searchInput.value = "";
+    search();
+}
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("searchList");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
